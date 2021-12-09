c55c8d85db0b79a6ce9ccabb3f57b381c15f7fa0
LOVINGFINE <lovingfine@gmail.com>
2021-12-09 15:24:45 +0800

    module rename

---
+++ packages/plugins/lib/route-helper/index.d.ts
@@ -0,0 +1,25 @@
+import { ReactElement } from "react";
+export interface DynamicRouteProviderProps {
+    routes: RouteItem[];
+}
+export interface RouteItem {
+    name: string;
+    exact?: boolean;
+    title?: string;
+    component?: string;
+    icon?: string | React.ReactElement;
+    path?: string;
+    redirect?: string;
+    routes?: Array<RouteItem>;
+    parent?: RouteItem;
+}
+export interface RouteConsumerOptions {
+    name: string;
+    exact?: boolean;
+    title?: string;
+    component?: string;
+    icon?: string | React.ReactElement;
+    redirect?: string;
+    parent?: RouteItem;
+}
+export declare const DynamicRouteProvider: ({ routes, }: DynamicRouteProviderProps) => ReactElement;
---
+++ packages/plugins/lib/route-helper/index.js
@@ -0,0 +1,12 @@
+import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
+import { lazy } from "react";
+import { Route, Routes, Outlet } from "react-router-dom";
+export const DynamicRouteProvider = ({ routes, }) => {
+    const render = (list) => {
+        return list.map((ele, i) => {
+            const LazyComponent = lazy(() => import(`@/${ele.component}`));
+            return (_jsx(Route, Object.assign({ path: ele.path || "*", element: _jsx(_Fragment, { children: ele.routes ? (_jsx(LazyComponent, Object.assign({ routes: ele.routes, path: ele.path }, { children: _jsx(Outlet, {}, void 0) }), void 0)) : (_jsx(LazyComponent, {}, void 0)) }, void 0) }, { children: ele.routes ? render(ele.routes) : undefined }), ele.name || "key-" + i));
+        });
+    };
+    return _jsx(Routes, { children: render(routes) }, void 0);
+};
